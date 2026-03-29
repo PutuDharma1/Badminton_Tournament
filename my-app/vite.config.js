ff814277887651b8cec435bf6d0b535de42dad46
PutuDharma1 <iputudharma.puspa@gmail.com>
2026-03-30 04:14:29 +0800

    aaa

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // TAMBAHKAN BARIS INI:
   define: {
-    global: 'window',
+    global: 'window'
   },
+  preview: {
+    host: true,
+    allowedHosts: ['badmintontournament-production.up.railway.app']
+  }
 })
